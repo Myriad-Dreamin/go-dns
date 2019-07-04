0b8912fb204765644e3b19ea125ad19e0e98e753
ST-Saint <st.saint.wyy@gmail.com>
2019-07-04 23:14:18 +0800

    support adding/getting msg to/from redis

--- msg/supfunc.go
+++ msg/supfunc.go
@@ -8,6 +8,26 @@ import (
 	mdnet "github.com/Myriad-Dreamin/go-dns/net"
 )
 
+var typename = map[uint16]string{
+	1:  "A",
+	2:  "NS",
+	3:  "MD",
+	4:  "MF",
+	5:  "CNAME",
+	6:  "SOA",
+	7:  "MB",
+	8:  "MG",
+	9:  "MR",
+	10: "NULL",
+	11: "WKS",
+	12: "PTR",
+	13: "HINFO",
+	14: "MINFO",
+	15: "MX",
+	16: "TXT",
+	28: "AAAA",
+}
+
 func BytesToInt(bs []byte, val interface{}) int {
 	var res int32
 	for i := 0; i < len(bs); i++ {
@@ -55,6 +75,51 @@ func GetFullName(bs []byte, offset int) ([]byte, int, error) {
 				}
 				if n == 0 {
 					return buf.Bytes(), cnt, nil
+				}
+				if i+n+1 >= len(bs) {
+					return nil, 0, errors.New("Index out of range")
+				}
+				for j := 0; j < n; j++ {
+					buf.WriteByte(bs[i+1+j])
+				}
+				i = i + n
+			}
+		}
+	}
+	return nil, 0, errors.New("Wrong Domain Name Format")
+}
+
+func GetStringFullName(bs []byte, offset int) ([]byte, int, error) {
+	var buf bytes.Buffer
+	var n, flag int
+	var cnt int
+	for k := 0; k < 1<<10; k++ {
+		if offset < 0 || offset >= len(bs) {
+			return nil, 0, errors.New("Index out of range")
+		}
+		for i := offset; ; i++ {
+			n = int(bs[i])
+			if n&0xc0 == 0xc0 {
+				if i+1 >= len(bs) {
+					return nil, 0, errors.New("Index out of range")
+				}
+				offset = n&0x3f<<8 + int(bs[i+1])
+				if k == 0 {
+					cnt += 2
+				}
+				break
+			} else {
+				//buf.WriteByte(bs[i])
+				if k == 0 {
+					cnt += n + 1
+				}
+				if n == 0 {
+					return buf.Bytes(), cnt, nil
+				}
+				if flag == 0 {
+					flag = 1
+				} else {
+					buf.WriteByte(byte('.'))
 				}
 				if i+n+1 >= len(bs) {
 					return nil, 0, errors.New("Index out of range")
